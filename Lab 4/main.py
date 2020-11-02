896259447c4abf34d5df674fc5d9bab078ee4d6d
ternovandarius <52597957+ternovandarius@users.noreply.github.com>
2020-11-02 19:04:00 +0200

    Add final implementation of scanner

--- Lab 4/main.py
+++ Lab 4/main.py
@@ -43,59 +43,77 @@ class SymbolTable:
 
     def lookup(self, key):
         hashed_key = self.hash(key)
-        return self.__symbols[hashed_key]
+        if self.__symbols[hashed_key]=="":
+            hashed_key = self.insert(key)
+        return hashed_key
 
     def __str__(self):
-        print(self.__symbols)
+        strr = ""
+        for i in range(self.__capacity):
+            if self.__symbols[i] != "":
+                strr += "[" + str(self.__symbols[i]) + ", " + str(i) + "]"
+        return strr
+
+class PIF:
+
+    def __init__(self):
+        self.pif=[]
+    def genPIF(self, token, index):
+        self.pif.append((token, index))
 
 class Scanner:
 
-    # This implementation is currently incomplete.
-
-    # TODO:
-    # add check if constant
-    # properly extract separators with regex
-    # make genPIF work
-
-
     def __init__(self, token):
-        self.st= SymbolTable(10)
-
+        self.st= SymbolTable(1000)
+        self.pif= PIF()
         tokens = open(token, "r")
         split_token = tokens.read().split("\n")
         self.reserved_words = []
         self.sep_op = []
-        for i in range(9, 21):
+        for i in range(20, 37):
             self.reserved_words.append(split_token[i])
-        for i in range(0, 9):
+        for i in range(0, 20):
             self.sep_op.append(split_token[i])
         tokens.close()
 
     def genPIF(self, token, index):
-        pass
+        self.pif.genPIF(token, index)
 
     def scan(self, filename):
         f = open(filename, "r")
         self.file_text = f.readlines()
         f.close()
-
         printed = False
         for i in range(0, len(self.file_text)):
             j = self.file_text[i].strip().split(" ")
+            new_j = []
             for k in j:
-                z = re.split()
-                print(k)
+                new_j.extend(re.split('([,;()[\]])', k))
+            for k in new_j:
+                if k=='':
+                    continue
+                #print(k)
                 if k in self.reserved_words or k in self.sep_op:
                     self.genPIF(k, 0)
-                elif re.match('[a-zA-Z][a-zA-Z0-9_]*', k):
+                elif re.match('^".*"$', k) or re.match('^\d*.?\d*$', k) or k=="true" or k=="false":
                     index = self.st.lookup(k)
-                    self.genPIF(k, index)
+                    self.genPIF("0", index)     #0 for constants
+                elif re.match('^[a-zA-Z][a-zA-Z0-9_]*$', k):
+                    index = self.st.lookup(k)
+                    self.genPIF("1", index)     #1 for identifiers
                 else:
                     print("Lexical error on line " + str(i+1) + ", \"" + k + "\"")
                     printed = True
         if not printed:
-            print("Lexically correct")
-
+            print("Lexically correct\n")
+            print("Symbol table: ")
+            print(self.st)
+            print("PIF:\n")
+            for i in self.pif.pif:
+                print(i)
+            print("\n")
+        else:
+            print("Lexical errors are present\n")
 
 
 y = Scanner("token.in")
